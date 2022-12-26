be35ebf8e51e45392111548d371981c832655462
David Gomez Muns <davidmuns@yahoo.es>
2022-12-26 12:11:48 +0100

    refactoring

--- car-service/src/main/java/com/microservice/carservice/repository/CarRepository.java
+++ car-service/src/main/java/com/microservice/carservice/repository/CarRepository.java
@@ -1,5 +1,6 @@
 package com.microservice.carservice.repository;
 
+
 import com.microservice.carservice.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
